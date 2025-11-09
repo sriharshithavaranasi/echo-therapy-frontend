534290ca24cfac1b48e6a75cc6594af1b7aadef2
sriharshithavaranasi <64347674+sriharshithavaranasi@users.noreply.github.com>
2025-11-08 22:44:42 -0500

    Turn off specific ESLint rules
    Disabled several ESLint rules for TypeScript and React.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,12 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "react/no-unescaped-entities": "off",
+      "prefer-const": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+    },
   },
 ];
 
